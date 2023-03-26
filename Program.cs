d24a6f01b543a720963e7278eec7d583fe5029db
ThinHippo <nsoy@mail.ru>
2023-03-26 16:45:22 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.Clear();
+    string intro = "Задача: написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше или равна 3-м символам.\n";
+    Console.WriteLine(intro);
+    string source = "Parma.txt";
+  }
+}
